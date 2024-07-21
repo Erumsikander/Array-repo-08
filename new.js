c6a46a0d607312ded466866d3cfbd19e7002ed99
Erum Sikander <mrsikanderghouri@gmail.com>
2024-07-22 02:05:43 +0500

    done

---
+++ new.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let arr = ["Saba", "Hina", "Noor", "Laiba", "Abida"];
+//    0        1         2        3         4
+//   -5     -4       -3     -2        -1
+console.log(arr[2]);
---
+++ new.ts
@@ -0,0 +1,5 @@
+let arr : string [] = ["Saba" , "Hina" , "Noor" , "Laiba" , "Abida"];
+                     //    0        1         2        3         4
+                     //   -5     -4       -3     -2        -1
+    
+console.log(arr[2]);             
